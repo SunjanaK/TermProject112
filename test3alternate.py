8a606c404bda4a6fb79deca38d6d6fe524ec11bd
SunjanaK <sunjana101@gmail.com>
2018-11-26 16:57:57 -0500

    smoother movement and less laggy, colors still weird tho

--- test3alternate.py
+++ test3alternate.py
@@ -10,6 +10,10 @@ import threading
 from tkinter import *
 from multiprocessing import Process
 
+listy = ["A","A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+
+#questions for TA's: how to make sure background noise doesn't influence machine, get rid of laggy moments
+#how to keep 2 notes from meeting bc currently, they could meet at a specific time, right?
 # PyAudio object.
 # got some pyaudio/aubio merge code from here:
 # https://github.com/aubio/aubio/issues/78
@@ -41,7 +45,7 @@ def tester(): #turn on microphone
         # Format the volume output so that at most
         # it has six decimal numbers.
         volume = "{:.6f}".format(volume)
-        # print(pitch)
+        print(pitch)
         if 290 < pitch < 300:
             return "D"
         elif 425 < pitch < 445 or int(pitch) == 219 :
@@ -64,16 +68,21 @@ def tester(): #turn on microphone
             return "G#"
         elif 460 < pitch < 470:
             return "A#"
-        elif 490 < pitch < 500:
+        elif 490 < pitch < 495:
             return "B"
+        elif pitch == 0.00:
+            return "Q"
 
-
+#only gives me points when it registers note correctly
+#how tester works: if any note is played other than note being checked, it stops
+#have to get it to re-record for every falling note if something is played, and make sure the correct note is played only in that instant
 def init(data):
     data.song = ["A", "A#", "B", "B#", "C", "C#", "D", "D#", "E"] #A string
     data.song2 = ["D", "D#", "E", "F", "F#", "G", "G#"] #D string
     data.song3 = ["G", "G#", "A", "A#", "B", "C", "C#"] #G string
     data.song4 = ['C', "C#", "D", "D#", "E", "F", "F#"] #C string
-    data.timerDelay = 1000
+    data.timerDelay = 100
+    data.counter = 0
     #these are the notes that will be checked off against what pyaudio hears
     data.play = [] #A string
     data.play2 = [] #D string
@@ -82,68 +91,110 @@ def init(data):
     #color of circle
     data.color = "purple"
     data.points = 0
-    #speed of various notes
-    data.speed = 20 #A string
-    data.speed2 = 20 #D string
-    data.speed3 = 20 #G string
-    data.speed4 = 40 #C string
+
 
 def timerFired(data):
+
     xcoord = 80 #C string
-    ycoord = 20
+    ycoord = 0
     x2coord = 160 #G string
     x3coord = 240 #D string
     x4coord = 320 #A string
 
-    if len(data.song4) > 0:
+    data.counter += 1
+    if data.counter % 20 == 0 and len(data.song4) > 0:
         data.play4.append([xcoord, ycoord, data.song4[0]])
         data.song4.pop(0)
-
-    if len(data.song3) > 0:
+    elif data.counter % 68 == 0 and len(data.song3) > 0:
         data.play3.append([x2coord, ycoord, data.song3[0]])
         data.song3.pop(0)
-    if len(data.song2) > 0:
+    elif data.counter % 85 == 0 and len(data.song2) > 0:
         data.play2.append([x3coord, ycoord, data.song2[0]])
         data.song2.pop(0)
-    if len(data.song) > 0:
+    elif data.counter % 100 == 0 and len(data.song) > 0:
         data.play.append([x4coord, ycoord, data.song[0]])
         data.song.pop(0)
 
     for item4 in data.play4:
-        item4[1] += data.speed4 #have letter move progressively down screen
+         # have letter move progressively down screen
 
-        if item4[1] >= 170:
-            if item4[2] == tester():
-                data.points += 1
+        if item4[1] >= 169:
+            sound4 = tester()
+            if sound4 == "Q" or sound4 != item4[2]:
+                data.color = "red"
+                print("hi")
+            elif sound4 == item4[2]:
                 data.color = "green"
-        if item4[1] > 190:
+                data.points += 1
+
+            data.play4.remove(item4)
+
+
+        else:
             data.color = "purple"
+            item4[1] += 5
 
     for item3 in data.play3:
-        item3[1] += data.speed3 #have letter move progressively down screen
+             # have letter move progressively down screen
 
+        if item3[1] >= 169:
+            sound3 = tester()
+            if sound3 == "Q" or sound3 != item3[2]:
+                data.color = "red"
+                print("hello")
+            elif sound3 == item3[2]:
+                data.color = "green"
+                data.points += 1
+
+            data.play3.remove(item3)
+
+
+        else:
+            data.color = "purple"
+            item3[1] += 5
     for item2 in data.play2:
-        item2[1] += data.speed2 #have letter move progressively down screen
+                 # have letter move progressively down screen
+
+        if item2[1] >= 169:
+            sound2 = tester()
+            if sound2 == "Q":
+                data.color = "red"
+            elif sound2 == item2[2]:
+                data.color = "green"
+                data.points += 1
+
+            else:
+                data.color = "red"
+            data.play2.remove(item2)
+
+
+        else:
+            data.color = "purple"
+            item2[1] += 5
+
 
     for item in data.play:
-        item[1] += data.speed #have letter move progressively down screen
+         #have letter move progressively down screen
 
-        if item[1] >= 170:
-            if item[2] == tester(): #testing to see if correct note is played
+        if item[1] >= 169:
+            sound = tester()
+            if sound == "Q":
+                data.color = "red"
+            elif sound == item[2]: #testing to see if correct note is played
             #when note hits purple circle
-                data.points += 1 #adds a point if correct note was played
                 data.color = "green" #circle turns green
-        if item[1] > 190:
-            data.color = "purple" #circle becomes purple after correct move
+                data.points += 1 #adds a point if correct note was played
+            else:
+                data.color = "red"
+            data.play.remove(item)
 
+        else:
+            data.color = "purple"
+            item[1] += 10
 
 def redrawAll(canvas, data):
     #creating the four circles
-    canvas.create_oval(70, 170, 90, 190, fill = data.color)
-    canvas.create_oval(150, 170, 170, 190, fill = data.color)
-    canvas.create_oval(230, 170, 250, 190, fill = data.color)
-    canvas.create_oval(310, 170, 330, 190, fill = data.color)
-
+    canvas.create_rectangle(0, 170, 400, 200, fill = data.color)
     canvas.create_text(50, 50, text = data.points)
     #creating the letters....
     for item4 in data.play4:
