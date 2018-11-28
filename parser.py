490d964279e1fd7475cef5201895f31dc5a17ffd
SunjanaK <sunjana101@gmail.com>
2018-11-27 19:47:43 -0500

    gets notes in order from sheet music

--- parser.py
+++ parser.py
@@ -42,10 +42,11 @@ import cv2
 import numpy as np
 
 #it's not reading the last half G for some reason????
-notesset = {"quartA.png","quartE.png", "halfG.png", "quartC.png", "halfA.png", "quartD.png", "quartB.png", "quartG.png", "halfD.png"}
+notesset = {"Aquart.png","Equart.png", "Ghalf.png", "Cquart.png", "Ahalf.png", "Dquart.png", "Bquart.png", "Gquart.png", "Dhalf.png"}
 img = cv2.imread("bigtwinkle.png")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 listy = []
+listyletter = []
 
 for note in notesset:
     template = cv2.imread(note, cv2.IMREAD_GRAYSCALE)
@@ -58,6 +59,9 @@ for note in notesset:
             if pt[0] not in points and pt[0] - 1 not in points and pt[0] - 2 not in points:
                 points.add(pt[0])
                 listy.append(pt)
+                listyletter.append(pt)
+                listyletter.append(note)
+
 
     elif "half" in note:
         loc = np.where(result >= 0.90)
@@ -65,22 +69,72 @@ for note in notesset:
             if pt[0] not in points and pt[0] - 1 not in points and pt[0] - 2 not in points:
                 points.add(pt[0])
                 listy.append(pt)
+                listyletter.append(pt)
+                listyletter.append(note)
+
+hundred = []
+twohundred = []
+threehundred = []
+fourhundred = []
+fivehundred = []
+ycoordslist = []
+yset = set()
+for coord in listy:
+    yset.add(coord[1])
+ylst = sorted(yset)
+for y in ylst:
+    if 0 < y <= 100:
+        hundred.append(y)
+    elif 100 < y <= 200:
+        twohundred.append(y)
+    elif 200 < y <= 300:
+        threehundred.append(y)
+    elif 300 < y <= 400:
+        fourhundred.append(y)
+    elif 400 < y <= 500:
+        fivehundred.append(y)
+
+hundredcoords = []
+twohundredcoords = []
+threehundredcoords = []
+fourhundredcoords = []
+fivehundredcoords = []
+
+for i in listy:
+    if i[1] in hundred:
+        hundredcoords.append(i)
+    elif i[1] in twohundred:
+        twohundredcoords.append(i)
+    elif i[1] in threehundred:
+        threehundredcoords.append(i)
+    elif i[1] in fourhundred:
+        fourhundredcoords.append(i)
+    elif i[1] in fivehundred:
+        fivehundredcoords.append(i)
 
 
+ycoordslist.append(sorted(hundredcoords, key=lambda k: [k[0]]))
+ycoordslist.append(sorted(twohundredcoords, key=lambda k: [k[0]]))
+ycoordslist.append(sorted(threehundredcoords, key=lambda k: [k[0]]))
+ycoordslist.append(sorted(fourhundredcoords, key=lambda k: [k[0]]))
+ycoordslist.append(sorted(fivehundredcoords, key=lambda k: [k[0]]))
+
+newlist = []
+for coo in ycoordslist:
+    if len(coo) > 0:
+        for val in coo:
+            newlist.append(val)
 
 
+alphabet = [] #this is all the notes of the song in order
+for lettercoord in newlist:
+    if lettercoord in listyletter:
+        c = listyletter.index(lettercoord)
+        alphabet.append(listyletter[c+1][0])
+alphabet.append("G")
+print(alphabet)
 
 
-
-
-
-
-
-print(listy)
-print(len(listy))
-
-
-cv2.imshow("img", img)
 
 
 cv2.waitKey(0)
